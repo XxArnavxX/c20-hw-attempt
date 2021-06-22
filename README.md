fe0a83ea6eb2902bfff05072c7413c4a4922ec0c
XxArnavxX <60574509+XxArnavxX@users.noreply.github.com>
2021-06-22 17:10:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c20-hw-attempt
+# p5.play-boilerplate
+CLASS 20 PROJECT BOILERPLATE
---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+
+  createCanvas(1200,400);
+}
+
+
+function draw(){
+  background = (250, 250, 250)
+
+  fill = (250, 200, 200)
+  ellipse = (mouseX, mouseY, 75, 20)
+}
